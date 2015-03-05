7af7d5adb0a4347ba185d776edf600be15064984
mattfaluotico <matt.faluotico@gmail.com>
2015-03-05 16:34:09 -0500

    fonts and lnks

--- app.rb
+++ app.rb
@@ -16,17 +16,21 @@ configure do
 end
 
 get '/' do
-  @news = getJSON('news')
+  erb :splash
+end
+
+get '/home' do
+  @news  = getJSON('news')
   @shows = getJSON('shows')
   erb :index
 end
 
 post '/contact' do
   puts "pretend email sent"
-  name = params[:name]
-  from = params[:email]
+  name  = params[:name]
+  from  = params[:email]
   phone =  params[:phone]
-  cont = params[:content]
+  cont  = params[:content]
 
   email = Mail.new do
       body  "#{cont} + ||| Contact at #{phone}}"
@@ -40,18 +44,23 @@ post '/contact' do
   email.deliver!  
 end
 
+not_found do
+  status 404
+  erb :splash
+end
+
 def get_news(event)
-  @day = event["day"]
-  @month = event["month"]
-  @title = event["title"]
+  @day     = event["day"]
+  @month   = event["month"]
+  @title   = event["title"]
   @content = event["content"]
   erb :_nentry
 end
 
 def get_show(event)
-  @day = event["day"]
-  @month = event["month"]
-  @title = event["location"]
+  @day     = event["day"]
+  @month   = event["month"]
+  @title   = event["location"]
   @content = event["description"]
   erb :_nentry
 end
