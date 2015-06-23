f95438874f8ce9254101179d77268e5d1d73a798
mattfaluotico <matt.faluotico@gmail.com>
2015-06-22 23:14:27 -0700

    fixed error

--- javascript/Content.Controller.js
+++ javascript/Content.Controller.js
@@ -5,9 +5,5 @@ var john = angular.module('johnpatrickhalling.content', [
 ]);
 
 john.controller('ContentController', [function($scope, $http){
-  var about = new Blog($scope, $http);
-  about.getAbout();
-  about.getRecent();
-}]);
 
-function getAbout
+}]);
