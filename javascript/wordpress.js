b6efb6739bbb30238784c933302de6434d132115
mattfaluotico <matt.faluotico@gmail.com>
2015-06-22 23:12:57 -0700

    added bower components for testing

--- javascript/wordpress.js
+++ javascript/wordpress.js
@@ -3,23 +3,38 @@
 var Blog = function($scope, $http) {
     this.$scope = $scope;
     this.$http = $http;
-    this.postURL = 'https://public-api.wordpress.com/rest/v1.1/sites/93818088/posts/'
+    this.postURL = 'https://public-api.wordpress.com/rest/v1.1/sites/93818088/posts/';
 
-    getPosts() {
-
-    }
+    // get the last 30 posts
+    getAllPosts() {
+      var postURL = this.postURL;
+      this.$http.get(postURL)
+        .success(function(response) {
+          $scope.posts = response.posts;
+        })
+        .error(function(){
+          this.$scope.summaries = [
+            "Unable to load posts",
+            "Unable to load posts",
+            "Unable to load posts",
+            "Unable to load posts",
+            "Unable to load posts",
+            "Unable to load posts"
+            ];
+        });
+    };
 
     getPost(postID) {
 
       var postsURL = this.postURL + postID;
       this.$http.get(postsURL)
-        .success(function(respose){
+        .success(function(response){
           this.$scope.postContent = response.content;
         })
         .error(function(){
           this.$scope.postContent = "Unable to load post";
         });
-    }
+    };
 
     getAbout() {
       var aboutPostURL= this.postURL + '1';
@@ -31,13 +46,13 @@ var Blog = function($scope, $http) {
         .error(function(){
           this.$scope.about = 'My name is John Patrck Halling. I play kick ass music in Kent, OH.'
         });
-    }
+    };
 
     getRecent() {
       var postsURL = this.postURL + 'pretty=true&number=5';
-      
+
       this.$http.get(postsURL)
-        .success(function(respose){
+        .success(function(response){
           this.$scope.summaries = response.posts;
         })
         .error(function(){
@@ -50,5 +65,5 @@ var Blog = function($scope, $http) {
             "Unable to load posts"
             ];
         });
-    }
-}
+    };
+};
