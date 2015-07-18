00ba117136db8337a745e28e9845882878622b7e
mattfaluotico <matt.faluotico@gmail.com>
2015-07-17 23:22:01 -0700

    update

--- javascript/app.js
+++ javascript/app.js
@@ -8,8 +8,6 @@ var app = angular.module('johnpatrickhalling', [
 
 app.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
 
-  $locationProvider.html5Mode(true);
-
   $routeProvider
     .when('/', {
       templateUrl: 'views/home.html',
@@ -22,30 +20,3 @@ app.config(['$routeProvider', '$locationProvider', function($routeProvider, $loc
     .otherwise({redirectTo: '/'});
 }]);
 
-
-// $(document).ready(function(){
-//   $("#l_Music").click(function(event) {
-//     event.preventDefault();
-//     $.scrollTo("#music", 500);
-//   });
-//   $("#l_Tour").click(function(event) {
-//     event.preventDefault();
-//     $.scrollTo("#tour", 500);
-//   });
-//   $("#l_Blog").click(function(event) {
-//     event.preventDefault();
-//     $.scrollTo("#blog", 500);
-//   });
-//   $("#l_Videos").click(function(event) {
-//     event.preventDefault();
-//     $.scrollTo("#videos", 500);
-//   });
-//   $("#l_About").click(function(event) {
-//     event.preventDefault();
-//     $.scrollTo("#about", 500);
-//   });
-//   $("#l_Contact").click(function(event) {
-//     event.preventDefault();
-//     $.scrollTo("#contact", 500);
-//   });
-// });
