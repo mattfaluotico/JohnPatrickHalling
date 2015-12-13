d0f6c4275f443df8207466ffefb7130b297954cf
mattfxyz <matt.faluotico@gmail.com>
2015-12-13 15:27:28 -0500

    moved videos to a new section

--- javascript/Content.Controller.js
+++ javascript/Content.Controller.js
@@ -8,32 +8,5 @@ var app = angular.module('johnpatrickhalling.content', [
 app.controller('ContentController', function($scope, Wordpress, $location) {
   Wordpress.getAbout($scope);
   Wordpress.getTour($scope);
-
-  $("#l_Music").click(function(event) {
-    event.preventDefault();
-    $.scrollTo("#music", 500);
-    location.hash = "music";
-    console.log($location.hash("music"));
-  });
-  $("#l_Tour").click(function(event) {
-    event.preventDefault();
-    location.hash = "tour";
-    $.scrollTo("#tour", 500);
-  });
-  $("#l_Videos").click(function(event) {
-    event.preventDefault();
-    location.hash = "video";
-    $.scrollTo("#videos", 500);
-  });
-  $("#l_About").click(function(event) {
-    event.preventDefault();
-    location.hash = "about";
-    $.scrollTo("#about", 500);
-  });
-  $("#l_Contact").click(function(event) {
-    event.preventDefault();
-    location.hash = "contact";
-    $.scrollTo("#contact", 500);
-  });
-
+  Wordpress.getVideos($scope);
 });
